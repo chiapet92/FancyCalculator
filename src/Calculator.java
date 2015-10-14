c93e224545687539fdbbae1e614b77c4714f1181
chiapet92 <markqi92@gmail.com>
2015-10-13 22:18:42 -0500

    moved all operations from the main body to the methods of the MyCalc class gave the user a choice immediately after an incorrect input to re-submit a valid input -> buggy for numbers, enters an infinite loop, i think it has something to do with the exception that's thrown.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,56 +1,29 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Calculator {
 
 	public static void main(String[] args) {
 	MyCalc simpleCalc = new MyCalc();	
-	Scanner keyboard = new Scanner(System.in);
-	double num= 0,x= 0,y = 0;
-	char operator = 0;
-	boolean end = false;
-	String error = "";
+	boolean dummyVal = false;
 	//Editable introductory messages
 	System.out.println("Welcome to Mark's Bitchin' Calculator!");
-	System.out.println("The mathematical operational will be completed in Reverse Polish Notation.");
-	System.out.println("The system will prompt you for each number and operator (only +,-,*, and / are available. Please don't try to break me.");
+	System.out.println("The mathematical operation will be completed in Reverse Polish Notation.");
+	System.out.println("The system will prompt you for each number and operator (only +,-,*, and / are available). Please don't try to break me.");
 	
+	// will prompt for all inputs individually
+	do {
+		dummyVal = simpleCalc.getInput(1); //firstNum
+	} while (dummyVal==false);
 	
-	// try block to obtain inputs and catch errors
-	try {
-		System.out.println();
-		System.out.println("Input first number:");
-		x= keyboard.nextDouble();
-		System.out.println("Input second number:");
-		y= keyboard.nextDouble();
-		System.out.println("Input operator (+,-,*,/):");
-		operator = (char)keyboard.next().charAt(0);
-	} catch (InputMismatchException e) {
-		error = "Input Mismatch Exception";
+	do {
+		dummyVal = simpleCalc.getInput(2); //secondNum
+	} while (dummyVal==false);	
+	
+	do {
+		dummyVal = simpleCalc.getInput(3); //Operator
+	} while (dummyVal==false);
+	
+	//perform calculation
+	simpleCalc.performCalculation();
+	simpleCalc.printAns();
+	simpleCalc.closeKeyboard();
 	}
-	
-	//switch case to determine what mathematical operation to do
-	switch (operator) {
-    case '+':  num = simpleCalc.add(x,y);
-             break;
-    case '-':  num = simpleCalc.subtract(x, y);
-             break;
-    case '*':  num = simpleCalc.mult(x, y);
-             break;
-    case '/':  num = simpleCalc.divide(x, y);
-             break;
-    default: error = "Invalid operator";
-             break;
-	}
-	
-	//outputting final answer, whether it be an error message or the # value
-	if (error.isEmpty()) {
-		System.out.println("Ans: " + num);
-	}
-	else {
-		System.out.println(error);
-	}
-	
-	keyboard.close();
-	}	
 }
--- src/MyCalc.java
+++ src/MyCalc.java
@@ -1,14 +1,81 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MyCalc {
+	double ans= 0,firstNum = 0,secondNum = 0;
+	char operator = 0;
+	Scanner keyboard = new Scanner(System.in);
+	public double getNum(int x) {
+		if (x ==1 ) {
+			return firstNum;
+		}
+		else {
+			return secondNum;
+		}
+	}
 	public double add(double x, double y) {
-		return x+y;
+		ans = x+y;
+		return ans;
 	}
 	public double subtract(double x, double y) {
-		return x-y;	
+		ans = x-y;
+
+		return ans;
 	}
 	public double mult(double x, double y) {
-		return x*y;
+		ans = x*y;
+		return ans;
 	}
 	public double divide(double x, double y) {
-		return x/y;
+		ans = x/y;
+		return ans;
+	}
+	public boolean getInput(int x) {
+		// try block to obtain inputs and catch errors
+		try {
+			System.out.println();
+			switch (x) {
+			case 1:	System.out.print("Input first number: ");
+			//keyboard.reset();
+			firstNum= keyboard.nextDouble();
+			break;
+			case 2: System.out.print("Input second number: ");
+			//keyboard.reset();
+			secondNum = keyboard.nextDouble();
+			break;
+			case 3: System.out.print("Input operator (+,-,*,/): ");
+			operator = (char)keyboard.next().charAt(0);
+			if ((operator != '+')&operator !='-'&operator !='*'&operator !='/') {
+				//System.out.println();
+				System.out.println("Invalid Operator");
+				return false;
+			}
+			break;
+			}
+			return true;
+		} catch (InputMismatchException e) {
+			//System.out.println();
+			System.out.println("Input Mismatch Exception");
+			return false;
+		}
+	}
+	public void performCalculation() {
+		switch (operator) {
+	    case '+': add(firstNum,secondNum);
+	             break;
+	    case '-': subtract(firstNum, secondNum);
+	             break;
+	    case '*': mult(firstNum, secondNum);
+	             break;
+	    case '/': divide(firstNum, secondNum);
+	             break;
+		}
+	}
+	public void printAns() {
+		System.out.println();
+		System.out.println("Ans: " + ans);
+	}
+	public void closeKeyboard() {
+		keyboard.close();
 	}
 }
