17ea5de1f44165cef5fa227ab420f4ad7e077ef4
chiapet92 <markqi92@gmail.com>
2015-10-13 20:02:46 -0500

    V2 trying to commit

---
+++ src/MyCalc.java
@@ -0,0 +1,14 @@
+public class MyCalc {
+	public double add(double x, double y) {
+		return x+y;
+	}
+	public double subtract(double x, double y) {
+		return x-y;	
+	}
+	public double mult(double x, double y) {
+		return x*y;
+	}
+	public double divide(double x, double y) {
+		return x/y;
+	}
+}
